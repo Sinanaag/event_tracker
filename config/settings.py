de11ac88907210838ddca321646c1f13d32f29e3
Sinanaag <sinanaag@gmail.com>
2026-02-26 23:54:42 +0530

    Prepare for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$88&mp(0s&uxmh1le#o&a(en8*4(e6u=%gg32o0yz&5q@vgo(*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
